2c368a8bef0e5c9fba8d5ecce31bd922089fc97b
unknown <luka.vukmirovic21@gmail.com>
2021-07-13 21:09:39 +0200

    Initial Commit

---
+++ src/main/java/cinema/repository/KorisnikRepository.java
@@ -0,0 +1,16 @@
+package cinema.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cinema.model.Korisnik;
+
+@Repository
+public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
+
+    Optional<Korisnik> findFirstByKorisnickoIme(String korisnickoIme);
+
+    Optional<Korisnik> findFirstByKorisnickoImeAndLozinka(String korisnickoIme,String lozinka);
+}
